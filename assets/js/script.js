67a1eb25bf918b2ee7b716cc0759bf275df0017e
Evan Allen <ERAllen@student.fullsail.edu>
2025-05-23 15:11:05 -0400

    Proper page displays when icon clicked on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 let navLinks = document.querySelectorAll('nav ul li a');
 
-navLinks.forEach((item) =>{
-    console.log(item);
+navLinks.forEach((item) => {
+    console.log(item)
     item.addEventListener('click', function(){
-        document.querySelector('nav ul li a.active').classList.remove("active");
-        item.classList.add("active");
+        document.querySelector('nav ul li a.active').classList.remove('active')
+        item.classList.add('active')
+        document.querySelector('main > section.active').classList.remove('active')
+        document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active')
+        console.log(item.getAttribute('href'))
     })
    
 })
